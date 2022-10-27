15be8f369a5d91db7c391c9162f499004d7fe899
Charlie Beckler <47010787+cbeckler@users.noreply.github.com>
2022-10-27 10:41:58 -0500

    bubble chart

--- static/charts.js
+++ static/charts.js
@@ -105,7 +105,7 @@ function buildCharts(sample) {
     // 2. Create the layout for the bubble chart.
     var bubbleLayout = {
       title: 'Bacteria Cultures Per Sample',
-      xaxis: 'OTU ID'
+      xaxis: {title: 'OTU ID'}
     };
 
     // 3. Use Plotly to plot the data with the layout.
