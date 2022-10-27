db7cb3ab65cab30302e5d88855ce456c278a4eb8
Charlie Beckler <47010787+cbeckler@users.noreply.github.com>
2022-10-27 10:03:46 -0500

    bubble chart(?)

--- static/charts.js
+++ static/charts.js
@@ -89,5 +89,26 @@ function buildCharts(sample) {
     };
     // 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot("bar", barData, barLayout);
+
+    // 1. Create the trace for the bubble chart.
+    var bubbleData = [{
+      x: otuIDs,
+      y: sampleValues,
+      text: otuLabels,
+      mode: 'markers',
+      marker: {
+        size: sampleValues,
+        color: otuIDs
+      }
+  }];
+
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      title: 'Bacteria Cultures Per Sample',
+      xaxis: 'OTU ID'
+    };
+
+    // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
   });
-}
+};
